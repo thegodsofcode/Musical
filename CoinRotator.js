e5b61adde1038c83ec3a4dd764ba9c0b80e4bc1c
epsooraj <epsooraj4@gmail.com>
2015-06-04 23:43:16 +0530

    prefab and rotation code

---
+++ CoinRotator.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public var angle : float = 0;
+public var speed : float = 5;
+
+function Start () {
+	//speed = 1;
+}
+
+function Update () {
+
+		angle+=speed;
+			    	    
+	    transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+	    
+	    if(angle>360)
+	    	angle = 0;	
+	
+}
